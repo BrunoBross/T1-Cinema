f4d55dfe8f1e530675a30c1ed2db7746b95f96d2
brunobross <bbarreto18@gmail.com>
2021-08-11 20:55:20 -0300

    excessoes meus menus

--- limite/tela_ingresso.py
+++ limite/tela_ingresso.py
@@ -3,16 +3,24 @@
 class TelaIngresso:
 
 	def tela_opcoes(self):
-		print("\033[1;96m-------==X( INGRESSO )X==-------\033[0;0m")
-		print("Escolha uma opcao")
-		print("0 - Retornar")
-		print("1 - Incluir Ingresso")
-		print("2 - Alterar Ingresso")
-		print("3 - Listar Ingresso")
-		print("4 - Excluir Ingresso")
 
-		opcao = int(input("Escolha uma das opcoes:"))
-		return opcao
+		while True:
+			print("\033[1;96m-------==X( INGRESSO )X==-------\033[0;0m")
+			print("Escolha uma opcao")
+			print("0 - Retornar")
+			print("1 - Incluir Ingresso")
+			print("2 - Alterar Ingresso")
+			print("3 - Listar Ingresso")
+			print("4 - Excluir Ingresso")
+
+			try:
+				opcao = int(input("Escolha uma das opções:"))
+				if 4 >= opcao >= 0:
+					return opcao
+				else:
+					print('\033[1;31mDigite um número correto!\033[0;0m')
+			except ValueError:
+				print('\033[1;31mDigite um número correto!\033[0;0m')
 
 	def pega_dados_ingresso(self):
 		print("\033[1;96m-------==X( DADOS INGRESSO )X==-------\033[0;0m")
--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -6,15 +6,18 @@ class TelaSistema:
 			print("\033[1;96m-------==X( CINEMA )X==-------\033[0;0m")
 			print("Escolha uma opção:")
 			print("0 - Finalizar sistema")
-			print("1 - Poltronas")
+			print("1 - Ingressos")
 			print("2 - Sessões")
-			print("3 - Ingressos")
-			print("4 - Salas")
-			print("5 - Filmes")
-			print("6 - Gêneros")
+			print("3 - Salas")
+			print("4 - Filmes")
+			print("5 - Gêneros")
+			print("6 - Poltronas")
 
 			try:
 				opcao = int(input("Escolha uma das opções:"))
-				return opcao
+				if 6 >= opcao >= 0:
+					return opcao
+				else:
+					print('\033[1;31mDigite um número correto!\033[0;0m')
 			except ValueError:
 				print('\033[1;31mDigite um número correto!\033[0;0m')
