4948f325436f36247a0fa2fbbea6a4c807e0eb38
brunobross <bbarreto18@gmail.com>
2021-09-06 19:16:11 -0300

    correcao listagem e tela de adicionar filme

--- limite/tela_filme.py
+++ limite/tela_filme.py
@@ -28,15 +28,22 @@ class TelaFilme:
 
     def popup_lista_filme(self, filmes: list):
         sg.theme(tema)
-        col = [
-            [sg.Text('\n'.join(filmes), font=fonte, text_color='white')]
-        ]
+        if len(filmes) <= 0:
+            col = [
+                [sg.Text('Não há filmes cadastrados.', font=fonte, text_color='white')]
+            ]
+        else:
+            col = [
+                [sg.Text('\n'.join(filmes), font=fonte, text_color='white')]
+            ]
         layout = [
             [sg.Text('Filmes Cadastrados:', size=(0, 2), font=('Impact', 20), text_color='white')],
-            [sg.Column(col, size=(400, 360), scrollable=True)],
+            [sg.Column(col, size=(400, 200), scrollable=True)],
+            [sg.Button('Retornar', font=fonte, size=tamanho)]
         ]
-        window = sg.Window('Filmes Cadastrados', layout, size=(400, 360))
+        window = sg.Window('Filmes Cadastrados', layout, size=(400, 360), element_justification='c')
         window.Read()
+        window.Close()
 
     def lista_generos_do_filme(self, lista: list, filme: Filme):
         print(f'\n\t{filme.titulo}:')
@@ -44,23 +51,22 @@ class TelaFilme:
             print(f'* {genero.tipo}')
 
     def pega_dados_filme(self):
-        while True:
-            titulo = input("Título: ")
-            print('\n\033[1;96m' + titulo + '\033[0;0m\n')
-            while True:
-                try:
-                    certeza = int(input("Tem certeza do título?\n1 - Sim\n2 - Não\n3 - Cancelar\nDigite uma opção: "))
-                    if 3 >= certeza >= 0:
-                        if certeza == 1:
-                            return titulo
-                        elif certeza == 3:
-                            return None
-                        elif certeza == 2:
-                            break
-                    else:
-                        print('\n\033[1;31mDigite um número entre 1 e 3!\033[0;0m\n')
-                except ValueError:
-                    print('\n\033[1;31mDigite um número correto!\033[0;0m\n')
+
+        sg.theme(tema)
+        layout = [
+            [sg.Text('Incluir Filmes:', size=(0, 2), font=('Impact', 20), text_color='white')],
+            [sg.Text('Título:', font=fonte, text_color='white'), sg.InputText('', size=(100, 2), font=fonte)],
+            [sg.Text('')],
+            [sg.Text('Confirmar?', font=fonte, text_color='white')],
+            [sg.Submit('Sim', font=fonte), sg.Cancel('Não', font=fonte)]
+        ]
+        window = sg.Window('Filmes Cadastrados', layout, size=(400, 360), element_justification='c')
+        values = window.Read()
+        window.Close()
+        if values[0] == 'Sim':
+            return values[1][0]
+        else:
+            window.Close()
 
     def mostra_filme(self, dados_filme):
         print(
